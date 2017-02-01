87ba36fef286117ec6c1777e16abe529b097f8a6
ivantang424 <ivantang424@hotmail.com>
2017-02-01 18:59:38 +0800

    connection file

---
+++ conn/conn.php
@@ -0,0 +1,10 @@
+<?php
+	$hostname = "127.0.0.1";
+	$username = "root";
+	$pwd = "";           
+	$db = "fyp";
+	
+	$conn = mysqli_connect($hostname, $username, $pwd, $db)
+	or die(mysqli_connect_error());
+	mysqli_query($conn,"SET NAMES UTF8");//chinese
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php //this is a testing file
+	require_once("/conn/conn.php");
+	$sql = 'Select * From category';
+	$rs = mysqli_query($conn,$sql);
+	while($rc = mysqli_fetch_assoc($rs)){
+		echo $rc['cid'];
+		echo $rc['type']. '<br>';
+	}
+	
+?>
