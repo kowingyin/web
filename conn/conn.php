af33697d1ed00a79f8d54a73e8388254954cdde3
kwl <kwlkwl654@gmail.com>
2017-02-02 15:11:49 +0800

    set timezone

--- conn/conn.php
+++ conn/conn.php
@@ -1,10 +1,11 @@
 <?php
-	$hostname = "127.0.0.1";
-	$username = "root";
-	$pwd = "";           
-	$db = "fyp";
-	
+	$hostname = '127.0.0.1';
+	$username = 'root';
+	$pwd = '';
+	$db = 'fyp';
+
 	$conn = mysqli_connect($hostname, $username, $pwd, $db)
 	or die(mysqli_connect_error());
-	mysqli_query($conn,"SET NAMES UTF8");//chinese
+	mysqli_query($conn,'SET NAMES UTF8');//chinese
+	mysqli_query($conn, 'Asia/Hong_Kong	');
 ?>
