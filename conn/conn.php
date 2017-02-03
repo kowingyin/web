71229b2beff8f69785e576ed6f195b9663e20f32
kwl <kwlkwl654@gmail.com>
2017-02-03 19:29:37 +0800

    making sql statement method has been testing

--- conn/conn.php
+++ conn/conn.php
@@ -10,6 +10,47 @@
 	mysqli_query($conn, 'Asia/Hong_Kong	');
 ?>
 <?php
+/*
+	$tableName = table name
+	$colArr = table's columns name are showed, also ['*'] can show all
+	$joinTableArr = the tables to be joined
+	$joinColArr = (optional)
+	$joinOriginalColArr = $tableName's column to be joined with $joinColArr, the position shoud be same as $joinColArr.		e.g. $joinColArr[0] == $joinOriginalColArr[0]
+	$whereArr = where condition
+	$groupArr = group condition
+	$order
+	$startRow = the starting point of the first row
+	$endRow = the ending point of the last row
+*/
+function sqlSelect($tableName, $colArr, $joinTableArr, $joinColArr, $joinOriginalColArr, $where, $group, $order, $startRow, $endRow){
+	$colLengthOfColArr = count($colArr);	//	the length of $colArr
+	$sql = 'SELECT ';
+	for ($i=0; $i < $colLengthOfColArr; $i++) {
+		$sql += $colArr[i];
+		if ($colLengthOfColArr-1 != i) {
+			$sql += ', ';
+		}
+	}
+	$sql += ' FROM '.$tableName.' ';
+
+	//	if joinTable not null, it would add the joining statement appends to the end of $sql
+	if ($joinTableArr) {
+		$joinLength = count($joinTableArr);
+		for ($i=0; $i < $joinLength; $i++) {
+			$sql += 'JOIN '.$joinTableArr[$i].' ON '.$joinTableArr[$i].'.'.$joinColArr[$i].' = '.$tableName.'.'.$joinOriginalColArr[$i];
+		}
+	}
+	$sql += ($where)?$where:'';
+	$sql += ($group)?$group:'';
+	$sql += ($order)?$order:'';
+	if (!isset($startRow) && $endRow) {
+		$startRow = 0;
+	}
+	$sql += ($endRow)?'LIMIT '.$startRow.', '.$endRow:'';
+	return $sql;
+}
+ ?>
+<?php
 	/*	$colArr uses the names of column to print each row, it is a string array
 		$tableName is input a table name, it is a string
 		$joinTable is a array consist of the tables those have to join (optional)
@@ -18,15 +59,8 @@
 	*/
 	function printAsTable($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol){
 		$return = '';
-		$sql = "SELECT * FROM `".$tableName"` ";
+		$sql = sqlSelect($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol, null, null, null, null, null)
 
-		//	if joinTable not null, it would add the joining statement appends to the end of $sql
-		if (joinTable) {
-			$joinLength = count($joinTable);
-			for ($i=0; $i < $joinLength; $i++) {
-				$sql += 'JOIN '.$$joinTable[$i].' ON '.$joinTable[$i].'.'.$joinCol[$i].' = '.$tableName.'.'.$joinOriginalCol[$i];
-			}
-		}
 		$result = mysqli_query($conn, $sql) or die('Mysql error');
 		$return = '<table class="table table-hover">'
 
