3f3ce98f8639a1f73f8af66f00290d0b3e8ed9ec
kwl <kwlkwl654@gmail.com>
2017-02-05 15:04:01 +0800

    modified:   conn/conn.php

--- conn/conn.php
+++ conn/conn.php
@@ -1,16 +1,22 @@
 <?php
-	$hostname = '127.0.0.1';
-	$username = 'root';
-	$pwd = '';
-	$db = 'fyp';
-	static $conn;
+class connection{
+	private  static $hostname;
+	private  static $username;
+	private  static $pwd;
+	private  static $db;
+	private $conn;
 
-	$conn = mysqli_connect($hostname, $username, $pwd, $db)
-	or die(mysqli_connect_error());
-	mysqli_query($conn,'SET NAMES UTF8');//chinese
-	mysqli_query($conn, 'Asia/Hong_Kong	');
-?>
-<?php
+	public function __construct(){
+
+		$hostname = '127.0.0.1';
+		$username = 'root';
+		$pwd = '';
+		$db = 'fyp';
+		$this->conn = mysqli_connect($hostname, $username, $pwd, $db)	or die(mysqli_connect_error());
+		mysqli_query($this->conn,'SET NAMES UTF8');//chinese
+		mysqli_query($this->conn, 'Asia/Hong_Kong	');
+	}
+
 /*
 	$tableName = table name
 	$colArr = table's columns name are showed, also ['*'] can show all
@@ -23,68 +29,65 @@
 	$startRow = the starting point of the first row
 	$endRow = the ending point of the last row
 */
-function sqlSelect($tableName, $colArr, $joinTableArr, $joinColArr, $joinOriginalColArr, $where, $group, $order, $startRow, $endRow){
+public function sqlSelect($tableName, $colArr, $joinTableArr, $joinColArr, $joinOriginalColArr, $where, $group, $order, $startRow, $endRow){
 	$colLengthOfColArr = count($colArr);	//	the length of $colArr
 	$sql = 'SELECT ';
 	for ($i=0; $i < $colLengthOfColArr; $i++) {
-		$sql += $colArr[$i];
+		$sql .= $colArr[$i];
 		if ($colLengthOfColArr-1 != $i) {
-			$sql += ', ';
+			$sql .= ', ';
 		}
 	}
-	$sql += ' FROM '.$tableName.' ';
+	$sql .= ' FROM '.$tableName.' ';
 
 	//	if joinTable not null, it would add the joining statement appends to the end of $sql
-	if ($joinTableArr) {
+	if (isset($joinTableArr)) {
 		$joinLength = count($joinTableArr);
 		for ($i=0; $i < $joinLength; $i++) {
-			$sql += 'JOIN '.$joinTableArr[$i].' ON '.$joinTableArr[$i].'.'.$joinColArr[$i].' = '.$tableName.'.'.$joinOriginalColArr[$i];
+			$sql .= '  JOIN '.$joinTableArr[$i].' ON '.$joinTableArr[$i].'.'.$joinColArr[$i].' = '.$tableName.'.'.$joinOriginalColArr[$i];
 		}
 	}
-	$sql += ($where)?$where:'';
-	$sql += ($group)?$group:'';
-	$sql += ($order)?$order:'';
-	if (!$startRow && $endRow) {
+	// echo $sql;
+	$sql .= (isset($where))?$where:'';
+	$sql .= (isset($group))?$group:'';
+	$sql .= (isset($order))?$order:'';
+	if (!isset($startRow) && isset($endRow)) {
 		$startRow = 0;
+		$sql .= 'LIMIT '.$startRow.', '.$endRow;
 	}
-	$sql += ($endRow)?'LIMIT '.$startRow.', '.$endRow:'';
-	echo '<script>console.log('.$sql.')</script>';
 	return $sql;
 }
- ?>
-<?php
 	/*	$colArr uses the names of column to print each row, it is a string array
 		$tableName is input a table name, it is a string
 		$joinTable is a array consist of the tables those have to join (optional)
 		$joinCol is a array includes the col name (optional)
 		$joinOriginalCol is a array includes the col names those for joining
 	*/
-	function printAsTable($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol){
+	public function printAsTable($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol){
 		$return = '';
-		$sql = sqlSelect($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol, null, null, null, null, null);
+		$sql = $this->sqlSelect($tableName, $colArr, $joinTable, $joinCol, $joinOriginalCol, null, null, null, null, null);
+		echo $sql.'<br />';
+		$result = mysqli_query($this->conn, $sql) or die('Mysql error');
 
-		$result = mysqli_query($conn, $sql) or die('Mysql error');
 		$return = '<table class="table table-hover">';
 
-
 		while($row = mysqli_fetch_array($result)){
-			$return += '<tr><td>'.$row[0].'</td></tr>';
+			$return .= '<tr><td>'.$row[0].'</td></tr>';
 		}
-		$return += '</table>';
+		$return .= '</table>';
 
-		closeSqlConn($result);
+		$this->closeSqlConn($result);
 		return $return;
 	}
-?>
-<?php
-function printAsTable($tableName, $colArr){
+public function printAsTableShort($tableName, $colArr){
 	printAsTable($tableName, $colArr, null, null, null);
 
 }
- ?>
- <?php
- function closeSqlConn($result){
+ public function closeSqlConn($result){
 	 mysqli_free_result($result);
-	 mysqli_close($conn);
+	 mysqli_close($this->conn);
  }
-  ?>
+
+}
+
+?>
