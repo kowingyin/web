153850fd16d0d5d218c5154c870df5d57d66fb58
kwl <kwlkwl654@gmail.com>
2017-02-09 18:08:05 +0800

    placed a login function

--- index.php
+++ index.php
@@ -1,12 +1,68 @@
+<!doctype html>
+<html>
+<head>
+	<title>Staff Phoning Centre</title>
+	<link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="#">Project Name</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+          <li class="active"><a href="#"><i class="icon-home icon-white"></i> Home</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+          <li><a href="#">Link</a></li>
+           <li class="dropdown">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+             <ul class="dropdown-menu">
+               <li><a href="#">Action</a></li>
+               <li><a href="#">Another action</a></li>
+               <li><a href="#">Something else here</a></li>
+               <li class="divider"></li>
+               <li class="nav-header">Nav header</li>
+               <li><a href="#">Separated link</a></li>
+               <li><a href="#">One more separated link</a></li>
+             </ul>
+          </li>
+        </ul>
+        <ul class="nav pull-right">
+          <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
+            <div class="dropdown-menu" style="padding:17px;">
+              <form class="form" id="formLogin">
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <button type="button" id="btnLogin" class="btn">Login</button>
+              </form>
+            </div>
+          </li>
+        </ul>
+      </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+  </div><!-- /.navbar-inner -->
+</div><!-- /.navbar -->
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script src="./js/login.js"></script>
+</html>
 <?php //this is a testing file
-	require_once("/conn/conn.php");
-	$sql = 'Select * From category';
-	$rs = mysqli_query($conn,$sql);
-	while($rc = mysqli_fetch_assoc($rs)){
-		echo $rc['cid'];
-		echo $rc['type']. '<br>';
-	}
-	
-	printAsTable2('category','*');
-	
+	// require_once("/conn/conn.php");
+	// $sql = 'Select * From category';
+	// $rs = mysqli_query($conn,$sql);
+	// while($rc = mysqli_fetch_assoc($rs)){
+	// 	echo $rc['cid'];
+	// 	echo $rc['type']. '<br>';
+	// }
+	//
+	// printAsTable2('category','*');
+	//
 ?>
