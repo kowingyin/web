1748f29a11e5640b0c9d5318595bba85efa96483
kwl <kwlkwl654@gmail.com>
2017-02-09 18:33:34 +0800

    fixed login button

--- index.php
+++ index.php
@@ -2,37 +2,18 @@
 <html>
 <head>
 	<title>Staff Phoning Centre</title>
-	<link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="./bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style>
+
+</style>
 <body>
 	<div class="navbar navbar-fixed-top">
   <div class="navbar-inner">
     <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
+
       <a class="brand" href="#">Project Name</a>
-      <div class="nav-collapse collapse">
-        <ul class="nav">
-          <li class="active"><a href="#"><i class="icon-home icon-white"></i> Home</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
-           <li class="dropdown">
-             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-             <ul class="dropdown-menu">
-               <li><a href="#">Action</a></li>
-               <li><a href="#">Another action</a></li>
-               <li><a href="#">Something else here</a></li>
-               <li class="divider"></li>
-               <li class="nav-header">Nav header</li>
-               <li><a href="#">Separated link</a></li>
-               <li><a href="#">One more separated link</a></li>
-             </ul>
-          </li>
-        </ul>
+      <div class="nav-collapse ">
         <ul class="nav pull-right">
           <li class="dropdown" id="menuLogin">
             <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
@@ -51,7 +32,7 @@
 </div><!-- /.navbar -->
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 <script src="./js/login.js"></script>
 </html>
 <?php //this is a testing file
