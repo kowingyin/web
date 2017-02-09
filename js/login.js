026c977405caecb052bfbbff2c81c20daa05d50e
kwl <kwlkwl654@gmail.com>
2017-02-09 20:09:56 +0800

    update login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
 $('button:#btnLogin').click(function() {
-	
+	$('#formLogin').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: '../login.php',
+        data: $(this).serialize(),
+        success: function(data) {
+            if (data === 'Login') {
+                window.location = '/user-page.php';
+            } else {
+                alert('Invalid Credentials');
+            }
+        }
+    });
+});
 });
